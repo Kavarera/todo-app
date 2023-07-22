0aaefc64d5d29e6eb7e55f901ebb383a5ae87d65
Kavarera <rafli.030715@gmail.com>
2023-07-22 22:48:53 +0700

    Create ui

---
+++ todo_app/lib/widget/itemListWidget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Itemlist extends StatelessWidget {
+  final String itemTitle, description;
+  const Itemlist(
+      {super.key, required this.itemTitle, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 5, 20, 5),
+      decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.black),
+        color: Colors.blue[100],
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(this.itemTitle),
+              Text(this.description),
+            ],
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                color: Colors.red,
+              ))
+        ],
+      ),
+    );
+  }
+}
